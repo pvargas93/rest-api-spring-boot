9c3cb3f9854aadceb291fa8e070f98c19b68d06f
Pablo.Vargasgit config --global user.name Pablo.Vargasgit config --global user.email pablovargas56@gmail.com <pablovargas56@gmail.com>
2025-11-04 22:02:05 -0300

    GreetingController

---
+++ api-example/src/main/java/br/edu/atitus/api_example/entities/UserEntity.java
@@ -0,0 +1,17 @@
+package br.edu.atitus.api_example.entities;
+
+import java.util.UUID;
+
+public class UserEntity {
+	
+	private UUID id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String password;
+	
+	
+
+}
